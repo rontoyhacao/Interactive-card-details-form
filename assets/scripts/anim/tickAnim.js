cecaebc984cc4acf8f4227d9dbf57e7082b87314
Ron Paolo <rptoyhacao@gmail.com>
2022-09-08 00:59:09 +0800

    path changes

--- assets/scripts/anim/tickAnim.js
+++ assets/scripts/anim/tickAnim.js
@@ -1,4 +1,4 @@
-import { anime } from '/node_modules/animejs/lib/anime.es.js';
+import anime from '/node_modules/animejs/lib/anime.es.js';
 export const timeline = anime.timeline();
 
 const container = document.getElementById('container');
